43d7ac60974dd8aed9be08ce17c1d6646e45b546
Jonny G <110258278+jonathanlevi14@users.noreply.github.com>
2022-10-01 14:54:46 -0700

    added a countdown clock

--- assets/script.js
+++ assets/script.js
@@ -1,26 +1,57 @@
-var timerEl = document.getElementById("countdown");
-function countdown() {
-  var timeLeft = 700;
+var questionsOneThroughTen = [
+  ["Question Number 2?", ["A: ONE", "B: TWO", "C: THREE", "D: FOUR"]],
+  ["Question Number 3?", ["A: ONE", "B: TWO", "C: THREE", "D: FOUR"]],
+  ["Question Number 1?", ["A: ONE", "B: TWO", "C: THREE", "D: FOUR"]],
+  ["Question Number 4?", ["A: ONE", "B: TWO", "C: THREE", "D: FOUR"]],
+  ["Question Number 5?", ["A: ONE", "B: TWO", "C: THREE", "D: FOUR"]],
+  ["Question Number 6?", ["A: ONE", "B: TWO", "C: THREE", "D: FOUR"]],
+  ["Question Number 7?", ["A: ONE", "B: TWO", "C: THREE", "D: FOUR"]],
+  ["Question Number 8?", ["A: ONE", "B: TWO", "C: THREE", "D: FOUR"]],
+  ["Question Number 9?", ["A: ONE", "B: TWO", "C: THREE", "D: FOUR"]],
+  ["Question Number 10?", ["A: ONE", "B: TWO", "C: THREE", "D: FOUR"]]
+]
 
-var timeInterval = setInterval(function () {
-  if (timeLeft > 1) {
-    timerEl.textContent = timeLeft + " seconds remaining";
-    timeLeft--;
-  // } else if (timeLeft <= 697) {
-  //   timerEl.textContent = timeLeft + " seconds remaining"
-  //   timerEl.style.color = "red";
-  //   timeLeft--;
+
+
+
+// var count = 0;
+// //  Select increment and decrement button elements
+// var  wincount = document.querySelector("#wincount");
+// // var countEl = document.querySelector("#");
+
+// // Updates count on page
+// function setCounterText() {
+//   wincount.textContent = count;
+// }
+// // Attach event listener to increment button element
+// wincount.addEventListener("click", function() {
+//   count++;
+//   setCounterText();
+//   console.log(wincount)
+// });
+
+// var timerEl = document.getElementById("countdown");
+// function countdown() {
+//   var timeLeft = 700;
+
+// var timeInterval = setInterval(function () {
+//   if (timeLeft > 1) {
+//     timerEl.textContent = timeLeft + " seconds remaining";
+//     timeLeft--;
+//   // } else if (timeLeft <= 697) {
+//   //   timerEl.textContent = timeLeft + " seconds remaining"
+//   //   timerEl.style.color = "red";
+//   //   timeLeft--;
   
-  } else if (timeLeft === 1) {
-    timerEl.textContent = timeLeft + " second remaining";
-    timeLeft--;
-  } else {
-    timerEl.textContent = '';
+//   } else if (timeLeft === 1) {
+//     timerEl.textContent = timeLeft + " second remaining";
+//     timeLeft--;
+//   } else {
+//     timerEl.textContent = '';
   
-    clearInterval(timeInterval);
-    // displayMessage();
-  }
-  console.log(timeLeft)
-}, 1000);
-}
-countdown();
+//     clearInterval(timeInterval);
+//     // displayMessage();
+//   }
+// }, 1000);
+// }
+// countdown();
